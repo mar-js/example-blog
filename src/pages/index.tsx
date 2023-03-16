547a9abf7f6bc0c99c0113850ccc9cd57aefda95
Martín <maguera37@gmail.com>
2023-03-16 18:46:04 -0300

    feat: I conected graphqlCMS in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,41 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 
-const Home: NextPage = () => <p>aaa</p>
+import { PostsContextModel } from 'contexts'
+
+import { CLIENT } from 'utils'
+
+import { queryGetPosts } from 'apis'
+
+import { TemplateHome } from 'components/templates'
+
+import { IPosts, IPostsContextModel } from 'interfaces'
+
+const Home: NextPage<IPostsContextModel> = (value) => (
+  <PostsContextModel.Provider value={ value }>
+    <TemplateHome />
+  </PostsContextModel.Provider>
+)
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const { posts }: IPosts = await CLIENT.request(queryGetPosts)
+
+    return {
+      props: {
+        loading: 'ok',
+        data: posts
+      }
+    }
+  } catch (error) {
+    console.error('ERROR SSR HOME', error)
+
+    return {
+      props: {
+        loading: 'fail',
+        error
+      }
+    }
+  }
+}
 
 export default Home
