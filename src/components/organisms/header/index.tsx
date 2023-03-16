1f1459854e51362958949ccc818d8b7537bc814b
Martín <maguera37@gmail.com>
2023-03-16 18:43:43 -0300

    feat: I created header component in organisms

---
+++ src/components/organisms/header/index.tsx
@@ -0,0 +1,7 @@
+import { Navigation } from 'components/molecules'
+
+export const Header: React.FC = () => (
+  <header className="container mx-auto mb-6 border-b-2">
+    <Navigation />
+  </header>
+)
