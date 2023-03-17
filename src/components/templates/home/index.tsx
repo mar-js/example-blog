87df00d36135ce4378e7d8da8e51de4d843cafa1
Martín <maguera37@gmail.com>
2023-03-17 18:35:54 -0300

    fix: I removed header and changed linkPost to slug

--- src/components/templates/home/index.tsx
+++ src/components/templates/home/index.tsx
@@ -1,16 +1,13 @@
-import { Card } from 'components/molecules'
-import { Header } from 'components/organisms'
 import { usePosts } from 'contexts'
+
+import { Card } from 'components/molecules'
 
 export const TemplateHome: React.FC = () => {
   const { data } = usePosts()
 
   return (
-    <>
-      <Header />
-      <main className="grid grid-cols-auto gap-2 px-3">
-        { data?.map(post => <Card key={ post.linkPost } { ...post } />) }
-      </main>
-    </>
+    <main className="grid grid-cols-auto gap-2 px-3">
+      { data?.map(post => <Card key={ post.slug } { ...post } />) }
+    </main>
   )
 }
