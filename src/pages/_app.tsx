68082870c87e9d643622ac39d9dfd08e858e544d
Martín <maguera37@gmail.com>
2023-03-17 18:36:58 -0300

    feat: I added header the layout of the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
 
+import { Header } from 'components/organisms'
+
 import 'styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component { ...pageProps } />
+  return (
+    <>
+      <Header />
+      <Component { ...pageProps } />
+    </>
+  )
 }
